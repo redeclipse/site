047d407859b81c080fc9eeeb5f49b502a2833a7b
Quinton Reeves <qreeves@gmail.com>
2015-04-30 23:14:38 +1000

    contribute links

--- bits/index.php
+++ bits/index.php
@@ -22,7 +22,8 @@
     $app['targets']['wiki'] = array('name' => 'Wiki', 'url' => 'http://redeclipse.net/wiki/', 'alturl' => 'http://redeclipse.net/wiki/', 'nav' => 1, 'redir' => 1);
     $app['targets']['faq'] = array('name' => 'FAQ', 'url' => 'http://redeclipse.net/wiki/FAQ_for_Red_Eclipse', 'nav' => 1, 'redir' => 1);
     $app['targets']['guide'] = array('name' => 'Guide', 'url' => 'http://redeclipse.net/wiki/Guide', 'nav' => 1, 'redir' => 1);
-    $app['targets']['install'] = array('name' => 'Install', 'url' => 'http://redeclipse.net/wiki/Howto_Install_Red_Eclipse', 'nav' => 1, 'redir' => 1);
+    $app['targets']['install'] = array('name' => 'Install', 'url' => 'http://redeclipse.net/wiki/How_to_Install_Red_Eclipse', 'nav' => 1, 'redir' => 1);
+    $app['targets']['contribute'] = array('name' => 'Contribute', 'url' => 'http://redeclipse.net/wiki/How_to_Contribute_to_Red_Eclipse', 'nav' => 1, 'redir' => 1);
     $app['targets']['servers'] = array('name' => 'Servers', 'url' => 'http://redflare.ofthings.net/', 'alturl' => 'http://redflare.ofthings.net/', 'nav' => 1, 'redir' => 1);
 
     $app['targets']['facebook'] = array('name' => 'Facebook', 'url' => 'http://www.facebook.com/redeclipse.net', 'nav' => 0, 'redir' => 1);
@@ -38,7 +39,6 @@
     $app['targets']['apply'] = array('name' => 'Apply', 'url' => 'http://docs.google.com/forms/d/1ZuvNFCIivYY_b5YLpCa_pnsbdR4oRdPplIkcbBexCDU/viewform', 'nav' => -1, 'redir' => 1);
     $app['targets']['guidelines'] = array('name' => 'Guidelines', 'url' => 'http://redeclipse.net/wiki/Multiplayer_Guidelines', 'nav' => -1, 'redir' => 1);
     $app['targets']['trademark'] = array('name' => 'Trademark', 'url' => 'http://redeclipse.net/wiki/Trademark_Policy', 'nav' => -1, 'redir' => 1);
-    $app['targets']['install'] = array('name' => 'Install', 'url' => 'http://redeclipse.net/wiki/How_to_install', 'nav' => -1, 'redir' => 1);
     $app['targets']['devel'] = array('name' => 'Devel', 'url' => 'http://redeclipse.net/wiki/Obtain_development_version', 'nav' => -1, 'redir' => 1);
     $app['targets']['license'] = array('name' => 'License', 'url' => 'http://redeclipse.net/wiki/License', 'nav' => -1, 'redir' => 1);
     $app['targets']['readme'] = array('name' => 'Readme', 'url' => 'http://redeclipse.net/wiki/Readme', 'nav' => -1, 'redir' => 1);
@@ -213,7 +213,7 @@
             </div>
             <div class="rightblock">
                 <h4>Support Us</h4>
-                <p id="donatemsg">Red Eclipse is developed by volunteers, and you get it free of charge; your contributions keep this project alive. You're encouraged to get involved with the community and help evolve the game, or <a href="/donate">donate to our worthy cause</a>.</p>
+                <p id="donatemsg">Red Eclipse is developed by volunteers, and you get it free of charge; your contributions keep this project alive. You're encouraged to get involved with the community and <a href="/contribute">help evolve the game</a>, or <a href="/donate">donate to our worthy cause</a>.</p>
             </div>
             <div class="endrightblock">&nbsp;</div>
             <div id="footer">
