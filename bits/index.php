37a1f11adfac534963bcd30afc55d4a74009341a
RemiClouet <remiclouet@MacBook-Air-de-Remi-Clouet.local>
2015-06-08 08:15:14 +0200

    fix button

--- bits/index.php
+++ bits/index.php
@@ -150,7 +150,7 @@
         	<h3>Create your own maps cooperatively online</h3>
         
   
-        	<div class="col-xs12 col-sm-12 col-md-6">
+        	<div class="col-xs12 col-sm-12 col-md-12">
         		<a href="/download/<?php echo $app['platform']; ?>" id="button" class="btn btn-primary"><p class="download"><span class="glyphicon glyphicon-download" aria-hidden="true"></span> FREE DOWNLOAD</p><p class="version"><?php echo $app['releasefull']; ?> released <?php echo $app['releasedate']; ?></p></a>
         	</div>
         
